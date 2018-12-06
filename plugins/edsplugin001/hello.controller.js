cc3a278646f35ac97f6fae8feb923649b4dccebd
Eddie Parker <edwparke@cisco.com>
2018-12-06 07:28:36 -0800

    Control the scale of the 3D with the cumulocity simulator

--- plugins/edsplugin001/hello.controller.js
+++ plugins/edsplugin001/hello.controller.js
@@ -3,9 +3,10 @@
 
   angular
     .module('myapp.hello')
-    .controller('HelloController', function ($scope, $http) {
+    .controller('HelloController', function ($scope, $http, $interval) {
 
     var vm = this;
+    var counter = 0;
 
 
     $scope.canvasWidth = 400;
@@ -14,31 +15,33 @@
     $scope.scale = 1;
     $scope.materialType = 'lambert';
 
-    $http.get('https://edsdemo.cumulocity.com/measurement/measurements?source=299&dateFrom=2018-12-04T14:20:20Z&dateTo=2018-12-04T14:20:25Z')
-        .then(function(response) {
-            vm.text = response.data;
-        }, function(response) {
-            vm.text = 'ERROR in http';
-        });
+    $interval(update_measurements, 1000, 0);
 
-    //vm.text = 'hello, world twice';
+    function update_measurements()
+    {
+        var dateTo = new Date(Date.now());
+        var dateFrom = new Date(dateTo.getTime());
+        dateFrom.setSeconds(dateTo.getSeconds() - 10);
+	var value = 0.5;
+
+// 'https://edsdemo.cumulocity.com/measurement/measurements?source=299&dateFrom=2018-12-04T14:20:20Z&dateTo=2018-12-04T14:20:25Z'
+
+
+        counter++;
+        $http.get('https://edsdemo.cumulocity.com/measurement/measurements?source=103&dateFrom=' + dateFrom.toISOString() + '&dateTo=' + dateTo.toISOString())
+            .then(function(response) {
+                value = response.data.measurements[response.data.measurements.length - 1].c8y_Temperature.T.value;
+                vm.text = counter.toString() + ' : ' + value.toString();
+                $scope.scale = (value + 10.0) / 20.0;
+                //vm.text = response.data;
+            }, function(response) {
+                vm.text = 'ERROR in http';
+            });
+        
+    }
 
   });
 
-//HelloController);
-
-//  function HelloController() {
-//    var vm = this;
-//
-//    vm.text = 'hello, world';
-
-//    webglContainer.canvasWidth = 400;
-//    webglContainer.canvasHeight = 400;
-//    webglContainer.dofillcontainer = true;
-//    webglContainer.scale = 1;
-//    webglContainer.materialType = 'lambert';
-
-//    }
 
 }());
 
